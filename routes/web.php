00acf8be06be3ca0a64ba0fdcd1e3b8e939c8734
Jieyab <71226310+Jieyab89@users.noreply.github.com>
2021-12-10 19:03:08 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', [HomeController::class, 'index']);
+Route::get('/home', function () {
+    return view('home');
+});
 

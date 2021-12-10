eaa880d100a8676f31092ff63ce3c50824c5f7b8
VinaFadillah <vinafadillah@student.telkomuniversity.ac.id>
2021-12-10 15:35:15 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!-- FOOTER LOPE -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <title>Home</title>
+</head>
+
+<body>
+    <!-- NAVBAR -->
+    <nav class="navbar navbar-expand-sm navbar-light bg-white">
+        <div class="container">
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav  mx-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="Home.php">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Booking.php">VACCINE</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Booking.php">PATIENT</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- AKHIR NAVBAR -->
+
+    <div class="container">
+        <h2><b>
+                <center> About Us </center>
+            </b></h2>
+        <div class="row justify-content-center" style="padding:50px">
+            <div class="col-4"> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO647bfreDF26G75uMnzZz_4cdorMUvUSWgQ&usqp=CAU" alt="Kecus-kecus">
+            </div>
+            <div class="col-5"> <a> Vaksin adalah produk biologi yang berisi antigen yang bila diberikan kepada seseorang akan menimbulkan kekebalan spesifik secara aktif terhadap penyakit tertentu. Produk/ zat yang dimasukkan (suntikan/ lewat mulut) kedalam tubuh untuk menstimulasi sistem imun tubuh. </a>
+            </div>
+        </div>
+
+</body>
+
+<!-- Modal Footer -->
+<div class="modal fade" id="footer" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog ">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="footer">Kesan Pesan Praktikum</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>Kesan : ketika TP WAD muncul, sangat menguji adrenalin karena rasanya akan bertempur 3 hari kedepan untuk hard effort. Alhamdulillah sekarang semuanya sudah selesai.
+                </p>
+                <p>Pesan : semoga untuk kedepannya waktu pengerjaan TP bisa diberi waktu lebih lama lagi supaya para praktikan bisa menyelesaikannya sebelum deadline pengumpulan. Terimakasih.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer class="card-footer nt-auto text-center bg-white border-0" data-bs-toggle="modal" data-bs-target="#footer" style="margin-top:27%">
+    <div class="container-fluid text-center">
+        <p>Made With <i class="bi bi-heart-fill" style="color:rgb(0, 111, 255);"></i> by Vina Fadillah_1202194208</p>
+    </div>
+
+</footer>
+
+</html>
